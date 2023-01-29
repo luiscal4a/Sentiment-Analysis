8210170aa02af9791d139533cc0fe85a0e177412
luiscal4a <luiscal4a@gmail.com>
2023-01-30 00:19:02 +0100

    Added setup files

---
+++ setup.sh
@@ -0,0 +1,6 @@
+mkdir Data
+pip install -r requirements.txt
+git clone https://github.com/x4nth055/emotion-recognition-using-speech
+mv emotion-recognition-using-speech ver_module
+cd ver_module
+pip install -r requirements.txt
